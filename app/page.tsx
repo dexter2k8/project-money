6265da2c838f9cf689e3435530e113b69615224f
Sidnei <neimestre@yahoo.com.br>
2026-06-19 22:18:25 -0300

    fix: updated project font and sign in and sign up route

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,60 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import {
+  CONTAINER,
+  CONTENT,
+  FORM,
+  PANEL_BASE,
+  signIn,
+  signUp,
+  toggleContainer,
+  togglePanel,
+} from "./constants";
+import { cx } from "class-variance-authority";
 
-export default function Home() {
+export default function SignIn() {
+  const [isSignIn, setIsSignIn] = useState(true);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main className={CONTAINER}>
+      <div className={CONTENT}>
+        <section className={signUp({ isSignIn })}>
+          <form className={FORM}>
+            <h1>Create Account</h1>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
+            <input type="text" placeholder="Avatar URL" />
+            <button type="submit">Create Account</button>
+          </form>
+        </section>
+
+        <section className={signIn({ isSignIn })}>
+          <form className={FORM}>
+            <h1>Sign In</h1>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Sign In</button>
+          </form>
+        </section>
+
+        <section className={toggleContainer({ isSignIn })}>
+          <div className={togglePanel({ isSignIn })}>
+            <div className={PANEL_BASE}>
+              <h1>Welcome Back!</h1>
+              <p>Enter your personal details to use all of site features</p>
+              <button onClick={() => setIsSignIn(false)}>Go to Sign In</button>
+            </div>
+
+            <div className={cx("right-0", PANEL_BASE)}>
+              <h1>Hello, Friend!</h1>
+              <p>Register with your personal details to use all of site features</p>
+              <button onClick={() => setIsSignIn(true)}>Go to Sign Up</button>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
