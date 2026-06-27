c66eaad3d8647fc211408f932f692d46a2afe171
Sidnei <neimestre@yahoo.com.br>
2026-06-27 14:16:34 -0300

    fix: updated table data borders

--- components/Table/components/Body/index.tsx
+++ components/Table/components/Body/index.tsx
@@ -10,9 +10,12 @@ export default function Body<T>({ rows, columns }: IBodyProps<T>) {
   return (
     <tbody>
       {rows.map((row, i) => (
-        <tr className="border-b border-neutral-200 hover:bg-neutral-100" key={i}>
+        <tr className="hover:bg-neutral-100" key={i}>
           {columns.map((col, j) => (
-            <td className={cx("p-2 whitespace-nowrap", col.className)} key={j}>
+            <td
+              className={cx("p-2 whitespace-nowrap border-b border-neutral-200", col.className)}
+              key={j}
+            >
               {col.render ? col.render(row[col.field]) : String(row[col.field])}
             </td>
           ))}
--- components/Table/components/Head/index.tsx
+++ components/Table/components/Head/index.tsx
@@ -8,18 +8,21 @@ export interface IHeadProps<T> {
 
 export default function Head<T>({ columns, caption }: IHeadProps<T>) {
   return (
-    <thead className="h-10 text-left border-b border-neutral-300 sticky top-0 bg-white">
+    <thead className="text-left sticky top-0 bg-white">
       {caption && (
-        <tr>
+        <tr className="h-10">
           <th className="px-2 whitespace-nowrap text-right font-semibold" colSpan={columns.length}>
             {caption}
           </th>
         </tr>
       )}
 
-      <tr>
+      <tr className="h-10">
         {columns.map((col, i) => (
-          <th className={cx("px-2 whitespace-nowrap", col.className)} key={i}>
+          <th
+            className={cx("border-b border-neutral-300 px-2 whitespace-nowrap", col.className)}
+            key={i}
+          >
             {col.renderHeader ? col.renderHeader(col.headerName) : col.headerName}
           </th>
         ))}
