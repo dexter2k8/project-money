bf94ccd79e464cb4ec0328122accdc7e4f4e5514
Sidnei <neimestre@yahoo.com.br>
2026-08-19 22:56:56 -0300

    fix: updated page title

--- app/(pages)/analytics/page.tsx
+++ app/(pages)/analytics/page.tsx
@@ -7,7 +7,7 @@ import YearlyBarChart from "./components/YearlyBarChart";
 export default function Analytics() {
   return (
     <div className="m-8 bg-white w-full rounded-2xl flex flex-col">
-      <h2 className="p-4">Analytics Content</h2>
+      <h2 className="p-4">Gráficos de Análise</h2>
       <div className="p-4 grid grid-cols-2 auto-rows-fr gap-5 flex-1 min-h-0">
         <div className="col-span-1 row-span-2 rounded min-h-0">
           <MonthlyBarChart title="Análise mensal" />
