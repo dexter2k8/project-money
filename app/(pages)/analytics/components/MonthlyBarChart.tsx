535add136818e721381886f6f0d5257f2981182e
Sidnei <neimestre@yahoo.com.br>
2026-08-19 22:40:58 -0300

    fix: fixed hydration error on loading charts

--- app/(pages)/analytics/components/MonthlyBarChart.tsx
+++ app/(pages)/analytics/components/MonthlyBarChart.tsx
@@ -1,4 +1,5 @@
 import { useMonthlyChartData } from "@/app/hooks/useMonthlyChartData";
+import { useMounted } from "@/app/hooks/useMounted";
 import ChartVerticalBar from "./ChartVerticalBar";
 
 interface IMonthlyBarChartProps {
@@ -7,8 +8,9 @@ interface IMonthlyBarChartProps {
 
 export default function MonthlyBarChart({ title }: IMonthlyBarChartProps) {
   const { days, credits, debits, saldo, isLoading } = useMonthlyChartData();
+  const mounted = useMounted();
 
-  if (isLoading) {
+  if (mounted && isLoading) {
     return (
       <div className="p-2 border border-neutral-200 w-full h-full flex items-center justify-center min-h-0 rounded">
         <span className="text-sm text-neutral-400">Carregando...</span>
--- app/(pages)/analytics/components/YearlyBarChart.tsx
+++ app/(pages)/analytics/components/YearlyBarChart.tsx
@@ -1,3 +1,4 @@
+import { useMounted } from "@/app/hooks/useMounted";
 import { useYearlyChartData } from "@/app/hooks/useYearlyChartData";
 import ChartVerticalBarYearly from "./ChartVerticalBarYearly";
 
@@ -7,8 +8,9 @@ interface IYearlyBarChartProps {
 
 export default function YearlyBarChart({ title }: IYearlyBarChartProps) {
   const { months, credits, debits, saldo, isLoading } = useYearlyChartData();
+  const mounted = useMounted();
 
-  if (isLoading) {
+  if (mounted && isLoading) {
     return (
       <div className="p-2 border border-neutral-200 w-full h-full flex items-center justify-center min-h-0 rounded">
         <span className="text-sm text-neutral-400">Carregando...</span>
